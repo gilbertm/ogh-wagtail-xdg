f6945d58ec8910a87cfa1886efb193c2fbd7f2da
root <root@Gilbert.DXB.SEE.local>
2024-12-10 12:52:09 +0400

    init

---
+++ home/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from wagtail.models import Page
+
+
+class HomePage(Page):
+    
+    """Template"""
+    template = "home/home_page.html"
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './templates/**/*.html', // Wagtail templates
+    './**/templates/**/*.html', // Django app templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
