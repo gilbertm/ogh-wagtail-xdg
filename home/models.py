933ea9968a8977c9c9bd34c1d394df709acae5f3
root <root@Gilbert.DXB.SEE.local>
2024-12-12 17:52:36 +0400

    form build

--- home/models.py
+++ home/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.shortcuts import render
+from django import forms
+from django.core.validators import RegexValidator
 
 from wagtail.models import Page
 
@@ -7,3 +10,70 @@ class HomePage(Page):
     
     """Template"""
     template = "home/home_page.html"
+
+    """Form processing"""
+    def serve(self, request):
+        if request.method == "POST":
+            form = CustomForm(request)
+            
+            if form.is_valid():
+                return render(request, self.template, {
+                    "page": self,
+                    "form": form,
+                    "success": True
+                })
+        else:
+            form = CustomForm()
+
+        return render(request, self.template, {
+            "page": self,
+            "form": form
+        })
+    
+
+
+class CustomForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        widget = forms.TextInput(attrs={
+            "placeholder": "Enter Your Name",
+            "class": "absolute top-9 left-0 font-mulish font-normal text-[#c1c1c1] text-sm tracking-[0] leading-[normal] !text-[13.1px] !top-[33px] bg-transparent w-[100%] d-block"
+            })
+        )
+    
+    email = forms.EmailField(
+        widget = forms.EmailInput(attrs={
+            "placeholder": "Enter Your Email"
+            })
+    )
+
+    phone = forms.CharField(
+        max_length=15,
+        validators = [RegexValidator(r'^\+?1?\d{9,15}$', 'Enter Your Phone')],
+        widget = forms.TextInput(attrs={
+            "placeholder": "Enter Your Phone",
+            "tyipe": "tel",
+            "class": "form-control"
+            })
+    )
+
+    date = forms.DateField(
+        widget = forms.DateInput(attrs={
+            "type": "date",
+            "class": "form-control"
+        })
+    )
+
+    time = forms.DateField(
+        widget = forms.TimeInput(attrs={
+            "type": "time",
+            "class": "form-control"
+        })
+    )
+
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            "placeholder": "Describe Your Problem"
+            })
+    )
+    
